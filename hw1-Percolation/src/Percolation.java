84d6a33df08c08b8509b81a59982d58383dfb890
Andrew Hsieh <andrew891221@gmail.com>
2022-09-25 18:22:03 +0800

    optimized code by reducing function call of uf.find(index)

--- hw1-Percolation/src/Percolation.java
+++ hw1-Percolation/src/Percolation.java
@@ -2,14 +2,14 @@ import edu.princeton.cs.algs4.Merge;
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//import java.io.IOException;
-//
-//import org.json.simple.JSONArray;
-//import org.json.simple.JSONObject;
-//import org.json.simple.parser.JSONParser;
-//import org.json.simple.parser.ParseException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 class Percolation {
@@ -38,17 +38,15 @@ class Percolation {
         firsts = new int[N * N]; // store first node of each connected component (linked list)
         lasts = new int[N * N]; // store last node of each connected component (linked list)
         percolatedRegions = new Point2D[N * N];
-        percolated = false;
+        percolated = false; // a flag that will be raised the on first percolation
 
-        int i = 0;
-        for (Node node : nodes) {
+        for (int i = 0; i < nodes.length; i++) {
             firsts[i] = i;
             lasts[i] = i;
-            node = new Node();
-            node.site = new Point2D(i / N, i % N);
-            node.isOpen = false;
-            nodes[i] = node;
-            i++;
+            nodes[i] = new Node();
+            nodes[i].site = new Point2D(i / N, i % N);
+            nodes[i].next = null;
+            nodes[i].isOpen = false;
         }
 
         for (int x = 1; x < size; x++) {
@@ -61,54 +59,59 @@ class Percolation {
     public void open(int i, int j) { // open site (row i, column j) if it is not open already
         int index = i * size + j;
         nodes[index].isOpen = true;
+        int find_index = uf.find(index);
 
         if (index - size >= 0 && nodes[index - size].isOpen) { // up
-            nodes[lasts[uf.find(index)]].next = nodes[firsts[uf.find(index - size)]];  // index node last -> new node first
-            int newFirst = firsts[uf.find(index)];  // index node first is the new first
+            nodes[lasts[find_index]].next = nodes[firsts[uf.find(index - size)]];  // index node last -> new node first
+            int newFirst = firsts[find_index];  // index node first is the new first
             int newLast = lasts[uf.find(index - size)]; // new node last is the new last
 
             uf.union(index, index - size);
-            ufTopConnected.union(index, index - size);
-            ufTopAndBottomConnected.union(index, index - size);
 
             firsts[uf.find(index)] = newFirst;
             lasts[uf.find(index)] = newLast;
+
+            ufTopConnected.union(index, index - size);
+            ufTopAndBottomConnected.union(index, index - size);
         }
         if (index + size < size * size && nodes[index + size].isOpen) { // down
-            nodes[lasts[uf.find(index)]].next = nodes[firsts[uf.find(index + size)]];  // index node last -> new node first
-            int newFirst = firsts[uf.find(index)];  // index node first is the new first
+            nodes[lasts[find_index]].next = nodes[firsts[uf.find(index + size)]];  // index node last -> new node first
+            int newFirst = firsts[find_index];  // index node first is the new first
             int newLast = lasts[uf.find(index + size)]; // new node last is the new last
 
             uf.union(index, index + size);
-            ufTopConnected.union(index, index + size);
-            ufTopAndBottomConnected.union(index, index + size);
 
             firsts[uf.find(index)] = newFirst;
             lasts[uf.find(index)] = newLast;
+
+            ufTopConnected.union(index, index + size);
+            ufTopAndBottomConnected.union(index, index + size);
         }
         if (index % size != 0 && nodes[index - 1].isOpen) { // left
-            nodes[lasts[uf.find(index)]].next = nodes[firsts[uf.find(index - 1)]];  // index node last -> new node first
-            int newFirst = firsts[uf.find(index)];  // index node first is the new first
+            nodes[lasts[find_index]].next = nodes[firsts[uf.find(index - 1)]];  // index node last -> new node first
+            int newFirst = firsts[find_index];  // index node first is the new first
             int newLast = lasts[uf.find(index - 1)]; // new node last is the new last
 
             uf.union(index, index - 1);
-            ufTopConnected.union(index, index - 1);
-            ufTopAndBottomConnected.union(index, index - 1);
 
             firsts[uf.find(index)] = newFirst;
             lasts[uf.find(index)] = newLast;
+
+            ufTopConnected.union(index, index - 1);
+            ufTopAndBottomConnected.union(index, index - 1);
         }
         if (index % size != size - 1 && nodes[index + 1].isOpen) { // right
-            nodes[lasts[uf.find(index)]].next = nodes[firsts[uf.find(index + 1)]];  // index node last -> new node first
-            int newFirst = firsts[uf.find(index)];  // index node first is the new first
+            nodes[lasts[find_index]].next = nodes[firsts[uf.find(index + 1)]];  // index node last -> new node first
+            int newFirst = firsts[find_index];  // index node first is the new first
             int newLast = lasts[uf.find(index + 1)]; // new node last is the new last
 
             uf.union(index, index + 1);
-            ufTopConnected.union(index, index + 1);
-            ufTopAndBottomConnected.union(index, index + 1);
 
             firsts[uf.find(index)] = newFirst;
             lasts[uf.find(index)] = newLast;
+
+            ufTopConnected.union(index, index + 1);
+            ufTopAndBottomConnected.union(index, index + 1);
         }
 
         if (percolates() && !percolated) {
@@ -166,135 +169,135 @@ class Percolation {
         return newPercolatedRegions;
     }
 
-//    public static void test(String[] args){
-//        Percolation g;
-//        JSONParser jsonParser = new JSONParser();
-//        try (FileReader reader = new FileReader(args[0])){
-//            JSONArray all = (JSONArray) jsonParser.parse(reader);
-//            int count = 0;
-//            for(Object CaseInList : all){
-//                count++;
-//                JSONArray a = (JSONArray) CaseInList;
-//                int testSize = 0; int waSize = 0;
-//                System.out.print("Case ");
-//                System.out.println(count);
-//                //Board Setup
-//                JSONObject argsSeting = (JSONObject) a.get(0);
-//                a.remove(0);
-//
-//                JSONArray argSettingArr = (JSONArray) argsSeting.get("args");
-//                g = new Percolation(
-//                        Integer.parseInt(argSettingArr.get(0).toString()));
-//
-//                for (Object o : a)
-//                {
-//                    JSONObject person = (JSONObject) o;
-//
-//                    String func =  person.get("func").toString();
-//                    JSONArray arg = (JSONArray) person.get("args");
-//
-//                    switch(func){
-//                        case "open":
-//                            g.open(Integer.parseInt(arg.get(0).toString()),
-//                                    Integer.parseInt(arg.get(1).toString()));
-//                            break;
-//                        case "isOpen":
-//                            testSize++;
-//                            String true_isop = (Boolean)person.get("answer")?"1":"0";
-//                            String ans_isop = g.isOpen(Integer.parseInt(arg.get(0).toString()),
-//                                    Integer.parseInt(arg.get(1).toString()))?"1":"0";
-//                            if(true_isop.equals(ans_isop)){
-//                                System.out.println("isOpen : AC");
-//                            }else{
-//                                waSize++;
-//                                System.out.println("isOpen : WA");
-//                            }
-//                            break;
-//                        case "isFull":
-//                            testSize++;
-//                            String true_isfu =  (Boolean)person.get("answer")?"1":"0";
-//                            String ans_isfu = g.isFull(Integer.parseInt(arg.get(0).toString()),
-//                                    Integer.parseInt(arg.get(1).toString()))?"1":"0";
-//                            if(true_isfu.equals(ans_isfu)){
-//                                System.out.println("isFull : AC");
-//                            }else{
-//                                waSize++;
-//                                System.out.println("isFull : WA");
-//                            }
-//                            break;
-//                        case "percolates":
-//                            testSize++;
-//                            String true_per = (Boolean)person.get("answer")?"1":"0";
-//                            String ans_per = g.percolates()?"1":"0";
-//                            if(true_per.equals(ans_per)){
-//                                System.out.println("percolates : AC");
-//                            }else{
-//                                waSize++;
-//                                System.out.println("percolates : WA");
-//                            }
-//                            break;
-//                        case "PercolatedRegion":
-//                            testSize++;
-//                            String true_reg = person.get("args").toString();
-//                            String reg = "[";
-//                            Point2D[] pr = g.PercolatedRegion();
-//                            for (int i = 0; i < pr.length; i++) {
-//                                reg = reg + ((int)pr[i].x() + "," + (int)pr[i].y());
-//                                if(i != pr.length - 1){
-//                                    reg =reg + ",";
-//                                }
-//                            }
-//                            reg = reg +"]";
-//                            if(true_reg.equals(reg)){
-//                                System.out.println("PercolatedRegion : AC");
-//                            }else{
-//                                waSize++;
-//                                System.out.println("PercolatedRegion : WA");
-//                            }
-//                            break;
-//                    }
-//
-//                }
-//                System.out.println("Score: " + (testSize-waSize) + " / " + testSize + " ");
-//            }
-//        }catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public static void test(String[] args){
+        Percolation g;
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader(args[0])){
+            JSONArray all = (JSONArray) jsonParser.parse(reader);
+            int count = 0;
+            for(Object CaseInList : all){
+                count++;
+                JSONArray a = (JSONArray) CaseInList;
+                int testSize = 0; int waSize = 0;
+                System.out.print("Case ");
+                System.out.println(count);
+                //Board Setup
+                JSONObject argsSeting = (JSONObject) a.get(0);
+                a.remove(0);
+
+                JSONArray argSettingArr = (JSONArray) argsSeting.get("args");
+                g = new Percolation(
+                        Integer.parseInt(argSettingArr.get(0).toString()));
+
+                for (Object o : a)
+                {
+                    JSONObject person = (JSONObject) o;
+
+                    String func =  person.get("func").toString();
+                    JSONArray arg = (JSONArray) person.get("args");
+
+                    switch(func){
+                        case "open":
+                            g.open(Integer.parseInt(arg.get(0).toString()),
+                                    Integer.parseInt(arg.get(1).toString()));
+                            break;
+                        case "isOpen":
+                            testSize++;
+                            String true_isop = (Boolean)person.get("answer")?"1":"0";
+                            String ans_isop = g.isOpen(Integer.parseInt(arg.get(0).toString()),
+                                    Integer.parseInt(arg.get(1).toString()))?"1":"0";
+                            if(true_isop.equals(ans_isop)){
+                                System.out.println("isOpen : AC");
+                            }else{
+                                waSize++;
+                                System.out.println("isOpen : WA");
+                            }
+                            break;
+                        case "isFull":
+                            testSize++;
+                            String true_isfu =  (Boolean)person.get("answer")?"1":"0";
+                            String ans_isfu = g.isFull(Integer.parseInt(arg.get(0).toString()),
+                                    Integer.parseInt(arg.get(1).toString()))?"1":"0";
+                            if(true_isfu.equals(ans_isfu)){
+                                System.out.println("isFull : AC");
+                            }else{
+                                waSize++;
+                                System.out.println("isFull : WA");
+                            }
+                            break;
+                        case "percolates":
+                            testSize++;
+                            String true_per = (Boolean)person.get("answer")?"1":"0";
+                            String ans_per = g.percolates()?"1":"0";
+                            if(true_per.equals(ans_per)){
+                                System.out.println("percolates : AC");
+                            }else{
+                                waSize++;
+                                System.out.println("percolates : WA");
+                            }
+                            break;
+                        case "PercolatedRegion":
+                            testSize++;
+                            String true_reg = person.get("args").toString();
+                            String reg = "[";
+                            Point2D[] pr = g.PercolatedRegion();
+                            for (int i = 0; i < pr.length; i++) {
+                                reg = reg + ((int)pr[i].x() + "," + (int)pr[i].y());
+                                if(i != pr.length - 1){
+                                    reg =reg + ",";
+                                }
+                            }
+                            reg = reg +"]";
+                            if(true_reg.equals(reg)){
+                                System.out.println("PercolatedRegion : AC");
+                            }else{
+                                waSize++;
+                                System.out.println("PercolatedRegion : WA");
+                            }
+                            break;
+                    }
+
+                }
+                System.out.println("Score: " + (testSize-waSize) + " / " + testSize + " ");
+            }
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String args[]) {
-//        test(args);
-        Percolation s = new Percolation(2);
-
-        System.out.println(s.isOpen(0,0));
-        System.out.println(s.isFull(0,0));
-        System.out.println(s.percolates());
-        s.PercolatedRegion();
-        System.out.println(" ");
-
-        s.open(0,0);
-        System.out.println(s.isOpen(0,0));
-        System.out.println(s.isFull(0,0));
-        System.out.println(s.percolates());
-        s.PercolatedRegion();
-        System.out.println(" ");
-
-        s.open(1,1);
-        System.out.println(s.isOpen(1,1));
-        System.out.println(s.isFull(1,1));
-        System.out.println(s.percolates());
-        s.PercolatedRegion();
-        System.out.println(" ");
-
-        s.open(0,1);
-        System.out.println(s.isOpen(0,1));
-        System.out.println(s.isFull(0,1));
-        System.out.println(s.percolates());
-        s.PercolatedRegion();
-        System.out.println(" ");
+        test(args);
+//        Percolation s = new Percolation(2);
+//
+//        System.out.println(s.isOpen(0,0));
+//        System.out.println(s.isFull(0,0));
+//        System.out.println(s.percolates());
+//        s.PercolatedRegion();
+//        System.out.println(" ");
+//
+//        s.open(0,0);
+//        System.out.println(s.isOpen(0,0));
+//        System.out.println(s.isFull(0,0));
+//        System.out.println(s.percolates());
+//        s.PercolatedRegion();
+//        System.out.println(" ");
+//
+//        s.open(1,1);
+//        System.out.println(s.isOpen(1,1));
+//        System.out.println(s.isFull(1,1));
+//        System.out.println(s.percolates());
+//        s.PercolatedRegion();
+//        System.out.println(" ");
+//
+//        s.open(0,1);
+//        System.out.println(s.isOpen(0,1));
+//        System.out.println(s.isFull(0,1));
+//        System.out.println(s.percolates());
+//        s.PercolatedRegion();
+//        System.out.println(" ");
     }
 }
